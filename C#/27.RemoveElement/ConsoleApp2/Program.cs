e4b33f9ff59bb5b6ec4172213b7b5e9da336920a
MoMoYuYus <125796776+MoMoYuYus@users.noreply.github.com>
2023-09-06 17:47:44 +0800

    Add files via upload
    给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
    不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
    元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
    说明:
    为什么返回数值是整数，但输出的答案是数组呢?
    请注意，输入数组是以「引用」方式传递的，这意味着在函数里修改输入数组对于调用者是可见的。
    你可以想象内部操作如下:
    // nums 是以“引用”方式传递的。也就是说，不对实参作任何拷贝
    int len = removeElement(nums, val);
    // 在函数里修改输入数组对于调用者是可见的。
    // 根据你的函数返回的长度, 它会打印出数组中 该长度范围内 的所有元素。
    for (int i = 0; i < len; i++) {
        print(nums[i]);
    }

---
+++ C#/27.RemoveElement/ConsoleApp2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+namespace RemoveElement
+{
+    class RemoveElement
+    {
+        public static void Main(string[] args)
+        {
+            string[] nums1;
+            int val;
+            Console.WriteLine("输入数组以空格分隔");
+            nums1 = Console.ReadLine().Split(' ');
+            Console.WriteLine("输入要移除的元素值");
+            val = Convert.ToInt32(Console.ReadLine());
+            int[] nums11 = new int[nums1.Length];
+            int num , p = 0;
+            Boolean flag;
+            foreach(string s in nums1)
+            {
+                flag = int.TryParse(s, out num);
+                if (flag)
+                {
+                    nums11[p++] = num;
+                }
+                else
+                {
+                    Console.WriteLine("数组中有非数值元素");
+                }
+            }
+            RemoveElement removeElement = new RemoveElement();
+            int len = removeElement.Remove(nums11, val);
+            for(int i = 0; i < len; i++)
+            {
+                Console.WriteLine(nums11[i]);
+            }
+        }
+
+        public int Remove(int[] nums, int val)
+        {
+            //1、暴力解法：时间复杂度：O(n^2) 空间复杂度：O(1)
+            /*
+            int size = nums.Length;
+            for(int i = 0; i < size; i++)
+            {
+                if (nums[i] == val)
+                {
+                    for(int j = i + 1; j < nums.Length; j++)
+                    {
+                        nums[j - 1] = nums[j];
+                    }
+                    i--;
+                    size--;
+                }
+            }
+            return size;
+            */
+
+            //2、双指针法（快慢指针法）：通过一个快指针和慢指针在一个for循环下完成两个for循环的工作。
+            // 时间复杂度：O(n)
+            // 空间复杂度：O(1)
+            //快指针：寻找新数组元素，新数组即是不含删除元素的数组。
+            //慢指针：指向更新 新数组下标的位置
+            int slowIndex = 0;
+            for(int fastIndex = 0; fastIndex < nums.Length; fastIndex++)
+            {
+                if(val != nums[fastIndex])
+                {
+                    nums[slowIndex++] = nums[fastIndex];
+                }
+            }
+            return slowIndex;
+        }
+    }
+}
