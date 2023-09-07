a33ee02478658f1b3e5c562f2ceeefa9c120ac0a
MoMoYuYus <125796776+MoMoYuYus@users.noreply.github.com>
2023-09-07 22:07:43 +0800

    删除有序数组中的重复项
    给你一个 升序排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。然后返回 nums 中唯一元素的个数。
    考虑 nums 的唯一元素的数量为 k ，你需要做以下事情确保你的题解可以被通过：
    更改数组 nums ，使 nums 的前 k 个元素包含唯一元素，并按照它们最初在 nums 中出现的顺序排列。nums 的其余元素与 nums 的大小不重要。
    返回 k 。

---
+++ C#/26.RemoveDuplicates/ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace RemoveDuplicates
+{
+    class RemoveDuplicates
+    {
+        public static void Main(string[] args)
+        {
+            string[] nums1;
+            Console.WriteLine("请输入待删除重复元素的数组以空格分隔");
+            nums1 = Console.ReadLine().Split(' ');
+            int[] nums = Array.ConvertAll(nums1, int.Parse);
+            RemoveDuplicates removeDuplicates = new RemoveDuplicates();
+            int k = removeDuplicates.Remove(nums);
+            for(int i = 0; i < k; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+        }
+
+        private int Remove(int[] nums)
+        {
+            int slowIndex = 0;
+            for(int fastIndex = 1; fastIndex < nums.Length; fastIndex++)
+            {
+                if (nums[slowIndex] != nums[fastIndex])
+                {
+                    nums[++slowIndex] = nums[fastIndex];
+                }
+            }
+            return slowIndex + 1;
+        }
+    }
+}
