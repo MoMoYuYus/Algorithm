3e611eb3406fbddc74b455fec54ded26a82917a9
MoMoYuYus <125796776+MoMoYuYus@users.noreply.github.com>
2023-09-07 22:11:58 +0800

    删除有序数组中的重复项 II
    给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使得出现次数超过两次的元素只出现两次 ，返回删除后数组的新长度。
    不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。

---
+++ C#/80.RemoveDuplicatesII/ConsoleApp4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace RemoveDuplicates
+{
+    class RemoveDuplicates
+    {
+        public static void Main(string[] args)
+        {
+            string[] nums1;
+            Console.WriteLine("输入待删除元素数组以空格分隔");
+            nums1 = Console.ReadLine().Split(' ');
+            int[] nums = Array.ConvertAll(nums1, int.Parse);
+            RemoveDuplicates removeDuplicates = new RemoveDuplicates();
+            int k = removeDuplicates.Remove(nums);
+            for(int i = 0; i < k; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+        }
+        // 快慢指针加可留存次数标识符（t<几，就是留存几次，通用于留存一个数组中可保留最多重复元素个数为任意）
+        private int Remove(int[] nums)
+        {
+            int slowIndex = 0;
+            int t = 1;
+            for(int fastIndex = 1; fastIndex < nums.Length; fastIndex++)
+            {
+                if (nums[slowIndex] != nums[fastIndex])
+                {
+                    nums[++slowIndex] = nums[fastIndex];
+                    t = 1;
+                }
+                else if (nums[slowIndex] == nums[fastIndex] && t < 2)
+                {
+                    nums[++slowIndex] = nums[fastIndex];
+                    t++;
+                }
+            }
+            return slowIndex + 1;
+        }
+    }
+}
