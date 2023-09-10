89c0556a63128059f345580621741a49308e89a3
MoMoYuYus <125796776+MoMoYuYus@users.noreply.github.com>
2023-09-10 11:46:51 +0800

    跳跃游戏
    给你一个非负整数数组 nums ，你最初位于数组的第一个下标 。数组中的每个元素代表你在该位置可以跳跃的最大长度。
    判断你是否能够到达最后一个下标，如果可以，返回 true ；否则，返回 false 。

---
+++ C#/55.CanJump/ConsoleApp9/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace CanJump
+{
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            string[] nums1;
+            Console.WriteLine("输入非负整数数组以空格分隔：");
+            nums1 = Console.ReadLine().Split(' ');
+            int[] nums = Array.ConvertAll(nums1, int.Parse);
+            Solution s = new Solution();
+            Boolean b = s.CanJump(nums);
+            Console.WriteLine("能否到达最后一个下标：{0}" , b);
+        }
+
+        private bool CanJump(int[] nums)
+        {
+            /*贪心算法：
+             *跳几步无所谓，关键在于可跳的覆盖范围！
+             *这个问题就转化为跳跃覆盖范围究竟可不可以覆盖到终点！
+             *贪心算法局部最优解：每次取最大跳跃步数（取最大覆盖范围），
+             *整体最优解：最后得到整体最大覆盖范围，看是否能到终点。
+            */
+            int cover = 0;
+            if (nums.Length == 0) return true; // 只有一个元素一定可以到达
+            //i 小于等于覆盖范围时继续循环
+            for(int i = 0; i <= cover; i++)
+            {
+                cover = Math.Max(i + nums[i], cover);
+                if(cover >= nums.Length - 1)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
