44ed5ef67e3b963de84c01b786203f8dc4af3a2d
MoMoYuYus <125796776+MoMoYuYus@users.noreply.github.com>
2023-09-29 09:03:22 +0800

    238.除自身以外数组的乘积
    给你一个整数数组 nums，返回 数组 answer ，其中 answer[i] 等于 nums 中除 nums[i] 之外其余各元素的乘积 。
    题目数据 保证 数组 nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
    请 不要使用除法，且在 O(n) 时间复杂度内完成此题。

---
+++ C#/238.ProductExceptSelf/ConsoleApp13/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ProductExceptSelf
+{
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            string[] nums1;
+            Console.WriteLine("输入数组以空格分隔");
+            nums1 = Console.ReadLine().Split(' ');
+            int[] nums = Array.ConvertAll(nums1, int.Parse);
+            Solution p = new Solution();
+            int[] answer = p.Product(nums);
+            foreach(int i in answer)
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+        private int[] Product(int[] nums)
+        {
+            //int[] answer = new int[nums.Length];
+            //int[] L = new int[nums.Length];
+            //int[] R = new int[nums.Length];
+            //L[0] = 1;
+            //for(int i = 1; i< nums.Length; i++)
+            //{
+            //    L[i] = nums[i - 1] * L[i - 1];
+            //}
+            //R[nums.Length - 1] = 1;
+            //for(int i = nums.Length - 2; i >= 0; i--)
+            //{
+            //    R[i] = nums[i + 1] * R[i + 1];
+            //}
+            //for(int i = 0; i < nums.Length; i++)
+            //{
+            //    answer[i] = L[i] * R[i]; 
+            //}
+
+            // 节约空间，简化版
+            int[] answer = new int[nums.Length];
+            answer[0] = 1;
+            // 存储左边乘积的结果
+            for(int i = 1; i < nums.Length; i++)
+            {
+                answer[i] = nums[i - 1] * answer[i - 1];
+            }
+            int R = 1;
+            for(int i = nums.Length - 1; i >= 0; i--)
+            {
+                //乘积结果等于左边乘积乘右边乘积R
+                answer[i] = answer[i] * R;
+                // 右边乘积R累乘
+                R *= nums[i];
+            }
+            return answer;
+        }
+    }
+}
