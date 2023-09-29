22369b787688aa9f649fcb0b9baa18c215e82b50
MoMoYuYus <125796776+MoMoYuYus@users.noreply.github.com>
2023-09-29 09:05:00 +0800

    加油站
    在一条环路上有 n 个加油站，其中第 i 个加油站有汽油 gas[i] 升。
    你有一辆油箱容量无限的的汽车，从第 i 个加油站开往第 i+1 个加油站需要消耗汽油 cost[i] 升。你从其中的一个加油站出发，开始时油箱为空。
    给定两个整数数组 gas 和 cost ，如果你可以按顺序绕环路行驶一周，则返回出发时加油站的编号，否则返回 -1 。如果存在解，则 保证 它是 唯一 的。

---
+++ C#/134.CanCompleteCircuit/ConsoleApp14/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace CanCompleteCircuit
+{
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            string[] gas1, cost2;
+            Console.WriteLine("输入加油站存油数组以空格分隔：");
+            gas1 = Console.ReadLine().Split(' ');
+            Console.WriteLine("输入开往下一个加油站消耗汽油数组以空格分隔：");
+            cost2 = Console.ReadLine().Split(' ');
+            int[] gas = Array.ConvertAll(gas1, int.Parse);
+            int[] cost = Array.ConvertAll(cost2, int.Parse);
+            Solution s = new Solution();
+            int num = s.CompleteCircuit(gas, cost);
+            Console.WriteLine("出发加油站编号为：{0}", num);
+        }
+
+        private int CompleteCircuit(int[] gas, int[] cost)
+        {
+            int curSum = 0;         // 实时剩余油量，当小于零时重置出发点
+            int totalSum = 0;       // 总剩余油量，如果小于零那一定走不完一圈
+            int start = 0;
+            for(int i = 0; i < gas.Length; i++)
+            {
+                curSum += gas[i] - cost[i];
+                totalSum += gas[i] - cost[i];
+                // 实时剩余油量小于零时重置出发点，重置剩余油量
+                if (curSum < 0)
+                {
+                    start = i + 1;
+                    curSum = 0;
+                }               
+            }
+            // 总剩余油量小于零那一定走不完一圈
+            if (totalSum < 0)
+            {
+                return -1;
+            }
+            return start;
+        }
+    }
+}
