8d76cc689298e3cbbd6da55c137d97521d2838c5
MoMoYuYus <125796776+MoMoYuYus@users.noreply.github.com>
2023-10-08 18:07:02 +0800

    分发糖果
    n 个孩子站成一排。给你一个整数数组 ratings 表示每个孩子的评分。
    你需要按照以下要求，给这些孩子分发糖果：
    每个孩子至少分配到 1 个糖果。
    相邻两个孩子评分更高的孩子会获得更多的糖果。
    请你给每个孩子分发糖果，计算并返回需要准备的 最少糖果数目 。
    
    示例 1：
    输入：ratings = [1,0,2]
    输出：5
    解释：你可以分别给第一个、第二个、第三个孩子分发 2、1、2 颗糖果。
    示例 2：
    
    输入：ratings = [1,2,2]
    输出：4
    解释：你可以分别给第一个、第二个、第三个孩子分发 1、2、1 颗糖果。
         第三个孩子只得到 1 颗糖果，这满足题面中的两个条件。

---
+++ C#/135.Candy/ConsoleApp15/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Candys
+{
+    class HandOutCandy
+    {
+        public static void Main(string[] args)
+        {
+            string[] ratings1;
+            Console.WriteLine("输入孩子评分数组以空格分隔：");
+            ratings1 = Console.ReadLine().Split(' ');
+            int[] ratings = Array.ConvertAll(ratings1, int.Parse);
+            var handOutCandy = new HandOutCandy();
+            int candyNum = handOutCandy.Candy(ratings);
+            Console.WriteLine("需要准备的最少糖果数为：{0}", candyNum);
+        }
+
+        private int Candy(int[] ratings)
+        {
+            int[] candyVec = new int[ratings.Length];
+            candyVec[0] = 1;
+            for(int i = 1; i < candyVec.Length; i++)
+            {
+                candyVec[i] = (ratings[i] > ratings[i - 1]) ? candyVec[i - 1] + 1 : 1 ;
+            }
+            for(int i = candyVec.Length - 2; i >= 0; i--)
+            {
+                if (ratings[i] > ratings[i + 1])
+                {
+                    candyVec[i] = Math.Max(candyVec[i], candyVec[i + 1] + 1);
+                }
+            }
+            int total = candyVec.Sum();
+            return total;
+        }
+    }
+}
