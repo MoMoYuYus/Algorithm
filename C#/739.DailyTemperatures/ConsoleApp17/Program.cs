99e87bcb5be81a687ae990c61aba4257144ceb8a
MoMoYuYus <125796776+MoMoYuYus@users.noreply.github.com>
2023-10-08 18:18:58 +0800

    每日温度
    给定一个整数数组 temperatures ，表示每天的温度，返回一个数组 answer ，其中 answer[i] 是指对于第 i 天，下一个更高温度出现在几天后。如果气温在这之后都不会升高，请在该位置用 0 来代替。
    
    示例 1:
    输入: temperatures = [73,74,75,71,69,72,76,73]
    输出: [1,1,4,2,1,1,0,0]
    
    示例 2:
    输入: temperatures = [30,40,50,60]
    输出: [1,1,1,0]
    
    示例 3:
    输入: temperatures = [30,60,90]
    输出: [1,1,0]

---
+++ C#/739.DailyTemperatures/ConsoleApp17/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace Temperatures
+{
+    class Temperature
+    {
+        static void Main(string[] args)
+        {
+            string[] temperatures1;
+            Console.WriteLine("输入每天的温度以空格分隔");
+            temperatures1 = Console.ReadLine().Split(' ');
+            int[] temperatures = Array.ConvertAll(temperatures1, int.Parse);
+            Temperature t = new Temperature();
+            int[] anwser = t.DailyTempertures(temperatures);
+            foreach (int i in anwser)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        private int[] DailyTempertures(int[] T)
+        {
+            int size = T.Length;
+            int[] answer = new int[size];
+            Array.Fill(answer, 0);
+            // 栈里存的元素为数组下标
+            Stack<int> s = new Stack<int>();
+            s.Push(0);
+            for(int i = 0; i < size; i++)
+            {
+                while (s.Count != 0 && T[i] > T[s.Peek()])
+                {
+                    answer[s.Peek()] = i - s.Peek();
+                    s.Pop();
+                }
+                s.Push(i);
+            }
+
+            //for (int i = 0; i < size; i++)
+            //{
+            //    if (T[i] < T[s.Peek()])  // 遍历元素T[i]小于栈顶元素s.Peek()
+            //    {
+            //        s.Push(i);
+            //    }
+            //    else if (T[i] == T[s.Peek()])  // 遍历元素T[i]等于栈顶元素s.Peek()
+            //    {
+            //        s.Push(i);
+            //    }
+            //    else    
+            //    {
+            //        // 循环弹出栈中小的值，这样才能保持栈头到栈底是递增顺序
+            //        while (s.Count != 0 && T[i] > T[s.Peek()]) // 遍历元素T[i]大于栈顶元素s.Peek()
+            //        {
+            //            answer[s.Peek()] = i - s.Peek();
+            //            s.Pop();
+            //        }
+            //        s.Push(i);
+            //    }
+            //}
+
+            return answer;
+        }
+    }
+}
